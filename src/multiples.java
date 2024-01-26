9345023e896783b85255106e2aac8b49a6147825
Tsebiso <tsebisonzinzama@gmail.com>
2024-01-26 12:24:23 +0200

    multiples

---
+++ src/multiples.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Tsebiso.Nzinzama
+ */
+public class multiples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num;
+        int sum = 0;
+       
+        for(int i = 3; i <= 10; i++){
+           system.out.print("please enter a number");
+           system.out.println(num);
+           
+           if(num/3 != 0 && num/5 != 0){
+               sum = sum + num;
+           }
+           else
+               system.out.println("please enter a valid number");
+            
+        }
+       
+        
+    }
+    
+}
